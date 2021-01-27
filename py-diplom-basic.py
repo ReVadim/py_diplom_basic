b72122ca4f4fac817dd01f9ec65229fab842f166
ReVadim <revani@yandex.ru>
2021-01-28 00:11:34 +0300

    all done

--- py-diplom-basic.py
+++ py-diplom-basic.py
@@ -1,18 +1,17 @@
 import requests
 import time
 import os
-import sys
-from pprint import pprint
+import shutil
+import datetime
 
-# ya_disk_api = input("input your REST API token: ")
-# owner_id = input("input your id: ")
+today = datetime.datetime.today().strftime("%Y-%m-%d-%S")
+my_path = f'C:/{today}/'
+ya_disk_api = input("Введите Ваш Яндекс API token: ")
+token = input("Введите Ваш VK token: ")
 
-with open('token.txt', 'r') as t, open('Ya_disk_api.txt', 'r') as y:
-    token = t.read().strip()
-    ya_disk_api = y.read().strip()
-
-ya_url = 'https://cloud-api.yandex.net/v1/disk/'
-mypath = 'C:/Folder/'
+# with open('token.txt', 'r') as t, open('Ya_disk_api.txt', 'r') as y:
+#     token = t.read().strip()
+#     ya_disk_api = y.read().strip()
 
 
 class VkSaver:
@@ -23,7 +22,7 @@ class VkSaver:
         self.version = '5.126'
         self.photo_stock = {}
         self.albums_list = {}
-        self.mypath = mypath
+        self.my_path = my_path
         self.params = {
             'access_token': self.token,
             'v': self.version,
@@ -40,12 +39,14 @@ class VkSaver:
             'user_id': owner_id,
         }
         response = requests.get(album_url, params={**self.params, **album_params}).json()['response']
-        time.sleep(0.4)
+        time.sleep(0.3)
         count_albums = response['count']
         print("Количество альбомов = ", count_albums)
         for items in response['items']:
             self.albums_list[str(items['id'])] = items['title']
-        print(self.albums_list)
+        for album_id, name in self.albums_list.items():
+            print(f"{album_id} - {name}")
+        # print(self.albums_list)
         return self.albums_list
 
     def get_photo(self, user_id=None, album_id=None):
@@ -53,6 +54,7 @@ class VkSaver:
             user_id = self.owner_id
         if album_id is None:
             album_id = 'profile'
+        print("Идёт подсчет количества фотографий в альбоме...")
         gp_params = {
             'user_id': user_id,
             'extended': 1,
@@ -60,26 +62,25 @@ class VkSaver:
             'album_id': album_id
         }
         response = requests.get(self.url + 'photos.get', params={**self.params, **gp_params}).json()['response']
-        time.sleep(0.4)
+        time.sleep(0.3)
         count_photos = response['count']
         for items in response['items']:
             self.photo_stock[items['id']] = [items['likes']['count'], items['sizes'][-1]['url']]
-            time.sleep(0.4)
-
+            time.sleep(0.3)
         print(f"В альбоме {count_photos} фото")
-        print(self.photo_stock)
+        # print(self.photo_stock)
         return self.photo_stock
 
     def save_photo(self, load_count: int):
+        print("\nЗагружаем во временную папку")
         i = 0
         photo_for_load = dict()
-        # mypath = 'C:/Folder/'
         try:
-            os.mkdir(self.mypath)
+            os.mkdir(self.my_path)
         except FileExistsError:
-            self.mypath = self.mypath.rstrip('/') + '-1/'  # work directory
-            mypath = self.mypath.rstrip('/') + '-1/'  # work directory
-            os.mkdir(self.mypath)
+            self.my_path = self.my_path.rstrip('/') + '-1/'  # work directory
+            my_path = self.my_path.rstrip('/') + '-1/'  # work directory
+            os.mkdir(self.my_path)
         for photo, url in self.photo_stock.items():
             if i < load_count and i <= len(self.photo_stock):
                 filename = url[1].split('/')[-1].split('?')[0]
@@ -88,45 +89,35 @@ class VkSaver:
         count = 1
         part_load = len(photo_for_load) / 10
         percent = round(100 / len(photo_for_load))
-        rmdr = 100 - (len(photo_for_load) * percent)
+        remains = 100 - (len(photo_for_load) * percent)
         for name, url in photo_for_load.items():
-            filepath = os.path.join(self.mypath, name)
+            file_path = os.path.join(self.my_path, name)
             part = int(count / part_load)
-            status = round(count * percent + rmdr)
+            download_view = round(count * percent + remains)
             r = requests.get(url, allow_redirects=True).content
-            with open(filepath, 'wb') as f:
+            with open(file_path, 'wb') as f:
                 f.write(r)
-            print(f"[{count} / {len(photo_for_load)}] {'##' * part + '--' * (10 - part)} {status} % || {name}\r")
+            print(f"[{count} / {len(photo_for_load)}] {'##' * part + '--' * (10 - part)} {download_view} % || {name}\r")
             time.sleep(.1)
             count += 1
         return "all photo downloaded"
-
-    # def make_dir(self):
-    #     """make dir"""
-    #     dir_name = 'test_dir'  # need to create
-    #     make_dir_url = ya_url + 'resources'
-    #     requests.put(make_dir_url,
-    #                  params={"path": f"{dir_name}"},
-    #                  headers={"Authorization": f"OAuth {Ya_disk_api}"}
-    #                  )
-    #     return f"Папка {dir_name} успешно создана"
 
 
 class YaUploader:
     def __init__(self):
         self.headers = {"Authorization": f"OAuth {ya_disk_api}"}
-        self.file_path = mypath
+        self.file_path = my_path
 
     def upload(self, dir_path=None):
         """Метод загруджает файлы из папки на яндекс диск"""
         if dir_path is None:
             dir_path = self.file_path
-        dir_name = dir_path.split('/')[-1]
-        print("dir name - ", dir_name)
+        dir_name = dir_path.split('/')[1]
         contents = []
+        print("\nЗагружаем на Yandex disk")
         for item in os.walk(dir_path):
             contents.append(item)
-        print(contents)
+        # print(contents)
         if contents:
             requests.put("https://cloud-api.yandex.net/v1/disk/resources",
                          params={"path": f"{dir_name}"},
@@ -171,6 +162,7 @@ class YaUploader:
             with open(f'{dir_path}', "rb") as f:
                 requests.put(href, files={"file": f})
                 complete = f"\nDownload file {dir_name} is complete"
+        shutil.rmtree(my_path)
         return print(complete)
 
 
